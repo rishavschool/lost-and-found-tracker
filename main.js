8a4b3d90d44593ec3fe87901e9f9ceb0a488b5cb
DarshGoel-07 <goeldarsh304@gmail.com>
2025-10-21 12:28:07 +0000

    added buttons

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-document.body.style.background = "#f0f0f0"
+let AddItemButton = document.getElementById("AddItemButton")
+let ViewItemButton = document.getElementById("ViewItemButton")
+let SearchItemButton = document.getElementById("SearchItemButton")
+let ReportItemButton = document.getElementById("ReportItemButton")
 
-console.log("Hello from extension")
+
+AddItemButton.addEventListener("click", (c) => {
+    console.log("add item")
+})
+ViewItemButton.addEventListener("click", (c) => {
+    console.log("add item")
+})
+SearchItemButton.addEventListener("click", (c) => {
+    console.log("add item")
+})
+ReportItemButton.addEventListener("click", (c) => {
+    console.log("add item")
+})
