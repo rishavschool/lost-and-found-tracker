f5a0038318e1ede08181e0a047f691bd57e65b75
nsolanki01 <144687546+nsolanki01@users.noreply.github.com>
2025-12-22 13:14:40 +0000

    feedback and credits

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,24 @@ let AddItemButton = document.getElementById("AddItemButton")
 let ViewItemButton = document.getElementById("ViewItemButton")
 let SearchItemButton = document.getElementById("SearchItemButton")
 let ReportItemButton = document.getElementById("ReportItemButton")
-
+let FeedbackButton = document.getElementById("FeedbackButton")
 
 AddItemButton.addEventListener("click", (c) => {
     console.log("add item");
     chrome.tabs.create({ url: "pages/add_item.html" });
 })
+
 ViewItemButton.addEventListener("click", (c) => {
     console.log("open item");
     chrome.tabs.create({ url: "pages/view_item.html" });
 })
+
 SearchItemButton.addEventListener("click", (c) => {
     console.log("search item");
     chrome.tabs.create({ url: "pages/search_item.html" });
+})
+
+FeedbackButton.addEventListener("click", (c) => {
+    console.log("feedback");
+    chrome.tabs.create({ url: "pages/feedback.html" });
 })
